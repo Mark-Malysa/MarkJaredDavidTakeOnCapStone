9676f2d1ea89fc83397b45dae30e348976507026
Mark-Malysa <marmal22@bergen.org>
2021-10-29 14:34:18 -0400

    first prototype

--- mjd_capstone/src/App.js
+++ mjd_capstone/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+//import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+  <tr>
+    <th>Model/Year</th>
+    <th>Location</th>
+    <th>Vin</th>
+    
+  </tr>
+  <tr>
+    <td>Audi A4 (2012)</td>
+    <td>Onsite: 15</td>
+    <td>123456789123456789</td>
+    <td><button>Edit</button></td>
+  </tr>
+  <tr>
+  <td>Audi Q7 (2012)</td>
+    <td>Onsite: 10</td>
+    <td>123456789123456789</td>
+    <td><button>Edit</button></td>
+  </tr>
+
+  <tr>
+
+  <td>Audi s4 (2014)</td>
+    <td>OffSite</td>
+    <td>123456789123456789</td>
+    <td><button>Edit</button></td>
+  </tr>
+</table>
+
+
+<button>Add Car</button>
+<button>Delete Car</button>
+
+<button onClick= {checkingButton}>Open Map</button>
+
     </div>
   );
+}
+
+function checkingButton(){
+  console.log("Working");
 }
 
 export default App;
---
+++ mjd_capstone/src/Map.js
@@ -0,0 +1,6 @@
+
+function Map() {
+    return (
+        <h1>hello</h1>
+    );
+}
