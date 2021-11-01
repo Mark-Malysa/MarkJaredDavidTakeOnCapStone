8f98df2d3a207cc833b81c74d0cffcba9ff2a07c
David Kriz <davkri22@bergen.org>
2021-11-01 11:31:58 -0400

    map shows up

--- mjd_capstone/src/Map.js
+++ mjd_capstone/src/Map.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
+import map from './prototype_map.png';
 
 function Map() {
   return (
     <div>
 
-    <img src='src/prototype_map.png' alt="Map"></img>
+    <img src={map}alt="Map"></img>
 
     <button onClick= {closeMap}>Close map</button>
     </div>
